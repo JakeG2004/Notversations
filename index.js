de22d1d5256fd8955ee42db22a0b7b4a19c50dc2
Jake <jake.lgendreau@gmail.com>
2024-01-01 11:30:07 +0000

    Added basic functionality

--- index.js
+++ index.js
@@ -1,61 +1,66 @@
 //Prerequisites
-const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits, AttachmentBuilder, EmbedBuilder } = require('discord.js');
-const { token, guildId, channelId } = require('./config.json');
+const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+    intents: [
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ]
+  });
 
 client.commands = new Collection();
 
-//Libraries
-var moment = require('moment-timezone');
-var cron = require("cron");
+//Create list of banned words
+var banned = [];
 
-function sendMessage(){
-	//Get date
-	let add = Math.floor(Math.random() * 2);
-	let day = parseInt(moment().tz("America/Los_Angeles").format().slice(8,10));
-	if(day + add <= 31){
-		day = day + add;
-	}
-	let month = parseInt(moment().tz("America/Los_Angeles").format().slice(5,7));
+//TODO: Add a configuration command that will allow the user to change percentage, print off the list of banned words, or clear the list of banned words.
 
-	//Get discord channel
-	let channel = client.guilds.cache.get(guildId).channels.cache.get(channelId);
-	
-	//If its November
-	if(month == 11){
-		channel.send("RRAAAHHHH THE BONEZONE SHALL DEPART. BONE YOU ALL NEXT YEAR :skull-1:");
-		throw new Error();
-	}
+//Get message
+client.on('messageCreate', (message) => {
+    //TODO: Make it so that the bot wont ban its own commands
 
-	//If its halloween
-	if(day == 31){
-		let wizzer = new AttachmentBuilder('pics/halloween.jpg', { name: 'halloween.png'})
-		channel.send({files: [wizzer] });
-	}
+    //Split message into array of strings
+    var words = message.content.split(" ");
+    
+    //Iterate through message, if banned word is found, delete message
+    for(var i = 0; i<words.length; i++){
+        if(banned.includes(words[i])){
+            //message.reply({content: 'Nuh uh', ephemeral: 'true'});
+            //TODO: Add "nuh uh" response
+            message.delete();
+        }
+    }
 
-	//Choose and send file
-	let pic = new AttachmentBuilder(`pics/output${day}.jpg`, { name: `output${day}.jpg` })
-	channel.send({ files: [pic] });
+    //5% chance that word gets banned
+    if((Math.floor(Math.random() * 20)) >= 10){
 
-	//Send message at random time
-	let min = 90 * 60;
-	let max = 720 * 60;
-	let rand = Math.floor(Math.random() * (max - min + 1) + min);
-	console.log('waiting ' + rand / 60 + ' minutes');
-	setTimeout(sendMessage, rand * 1000);
-}
+        //Choose word to ban
+        //TODO: Modify this so that it doesn't catch repeat words
+        var tmpWord = words[Math.floor(Math.random() * words.length)];
+
+        console.log("banned", tmpWord);
+
+        //Make sure that item is not already in the list, then add item to list of banned words
+        if(banned.includes(tmpWord) == false){
+            banned.push(tmpWord);
+        }
+
+        //Print out banned list
+        for (var i=0; i< banned.length; i++){
+            console.log(banned[i]);
+        }
+    }
+});
 
 client.on(Events.ClientReady, async () => {
 	
   //login notif
-  let channel = client.guilds.cache.get(guildId).channels.cache.get(channelId);
   console.log(`Logged in as ${client.user.tag}!`);
 
-  //channel.send("I'm sorry. It will happen again.");
-  sendMessage();
 });
 
 client.login(token);
