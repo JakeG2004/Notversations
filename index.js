9383eff7ec4b77f79953f939b733e24c662d3e45
Jake <jake.lgendreau@gmail.com>
2024-01-01 21:28:51 +0000

    qol

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Prerequisites
-const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, AttachmentBuilder, EmbedBuilder } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
@@ -14,14 +13,23 @@ const client = new Client({
 
 client.commands = new Collection();
 
+var cron = require("cron");
+
 //Create list of banned words
 var banned = [];
+
+//Reset list every 24 hours
+//idk if this actually works
+let reset = new cron.CronJob('0 0 * * *', () => {
+    banned = [];
+    console.log("List successfully reset");
+});
 
 //TODO: Add a configuration command that will allow the user to change percentage, print off the list of banned words, or clear the list of banned words.
 
 //Get message
 client.on('messageCreate', (message) => {
-    //TODO: Make it so that the bot wont ban its own commands
+
 
     //Split message into array of words stored as strings
     var words = message.content.split(" ");
@@ -29,9 +37,13 @@ client.on('messageCreate', (message) => {
     //Iterate through message, if banned word is found, delete message
     for(var i = 0; i<words.length; i++){
         if(banned.includes(words[i])){
-            //message.reply({content: 'Nuh uh', ephemeral: 'true'});
-            //TODO: Add "nuh uh" response
+
+            //message.reply("Nuh uh");
+            //TODO: Add "nuh uh" response. May not be possible as those must be replies
             message.delete();
+
+            //Break here or else it may try to delete an already deleted message
+            break;
         }
     }
 
@@ -39,27 +51,28 @@ client.on('messageCreate', (message) => {
     if((Math.floor(Math.random() * 20)) >= 10){
 
         //Choose word to ban
-        //TODO: Modify this so that it doesn't catch repeat words
         var tmpWord = words[Math.floor(Math.random() * words.length)];
 
-        console.log("banned", tmpWord);
-
-        //Make sure that item is not already in the list, then add item to list of banned words
+        //Make sure that the word is not already in the list, then add the word to list of banned words
         if(banned.includes(tmpWord) == false){
             banned.push(tmpWord);
         }
 
         //Print out banned list
         for (var i=0; i< banned.length; i++){
-            console.log(banned[i]);
+            process.stdout.write(banned[i] + ", ");
         }
+        process.stdout.write("\n")
     }
 });
 
 client.on(Events.ClientReady, async () => {
 	
-  //login notif
-  console.log(`Logged in as ${client.user.tag}!`);
+    //login notif
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    //Start reset thing
+    reset.start();
 
 });
 
